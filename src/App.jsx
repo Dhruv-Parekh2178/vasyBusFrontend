33dc1de2daa10e52d688eb384545fef25243d36f
Dhruv-Parekh2178 <dhruvparekh1229@gmail.com>
2026-03-13 18:22:45 +0530

    search Result page added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import SearchResults from "./pages/SearchResult";
-
+import SeatSelection from "./pages/SeatSelection";
+import ProtectedRoute from "./components/ProtectedRoute"; 
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
          <Route path="/search-results" element={<SearchResults />} />
+          <Route path="/seat-selection/:scheduleId" element={
+          <ProtectedRoute><SeatSelection /></ProtectedRoute>
+        } />
       </Routes>
     </>
   );
